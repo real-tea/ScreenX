02bc85d77d65168cf0826f946f4d7a34fab5a81d
real-tea <akash.0711a@gmail.com>
2022-11-20 14:12:42 +0530

    completed background color adding in screenshot

--- components/Toolbar/Tool/Color.js
+++ components/Toolbar/Tool/Color.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Color = ({bgColor , onClick}) => {
 
-    const divStyle = "inline-flex w-24 h-24 active:scale-95 rounded-full m-1 ring-2 ring-transparent shadow-md hover:ring-gray-300"  + bgColor ;     
+    const divstyle = "inline-flex w-24 h-24 active:scale-95 rounded-full m-1 ring-2 ring-transparent shadow-md hover:ring-gray-300"  + bgColor ;     
 
-    function onClick(){
+    function clickFun(){
         onClick()
-        localStorage.setItem('lastColor', getElementById('my-node').styles.backgroundImage);
+        localStorage.setItem('lastColor', document.getElementById('my-node').style.backgroundImage);
     }
   return (
     <div className={divstyle} onClick={clickFun}>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = withPWA(nextConfig);
